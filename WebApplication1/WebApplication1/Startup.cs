72453d4e891393343b9cfce49b9cdcbf24dff6c9
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-06-27 12:45:23 +0530

    added EF

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -26,6 +26,7 @@ namespace WebApplication1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +40,8 @@ namespace WebApplication1
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseAuthorization();
 
