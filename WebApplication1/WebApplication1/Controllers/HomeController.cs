d76ae33019b9586446d7e40e642211b1cd74be12
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-06-27 09:51:51 +0530

    first commit

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("show1")]
+        public string show()
+        {
+            return "show is getting called";
+        }
+
+        public string show2()
+        {
+            return "show 2 is getting called";
+        }
+    }
+}
