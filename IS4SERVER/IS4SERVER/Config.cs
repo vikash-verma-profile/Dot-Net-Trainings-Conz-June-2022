dabf3b2f4a7c545dd380f8bdf4e4d5ee3f90b717
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-07-13 17:25:23 +0530

    added validation

---
+++ IS4SERVER/IS4SERVER/Config.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IS4SERVER
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId()
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new ApiResource[] {
+            new ApiResource("api1","Api1 scope 1"),
+              new ApiResource("api2","Api1 scope 2")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new Client[] {
+
+                new Client
+                {
+                    ClientId="client",
+                    AllowedGrantTypes=GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes={"api1"},
+                },
+            };
+        }
+    }
+}
