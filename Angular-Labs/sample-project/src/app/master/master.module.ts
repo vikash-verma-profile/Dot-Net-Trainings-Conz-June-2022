a62b5a04dd6ed4482e8cd7ce710fc98dce0995f8
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-07-14 11:54:36 +0530

    added

--- Angular-Labs/sample-project/src/app/master/master.module.ts
+++ Angular-Labs/sample-project/src/app/master/master.module.ts
@@ -9,6 +9,7 @@ import { LoginComponent } from '../login/login.component';
 import { RegisterComponent } from '../register/register.component';
 import { Mainroutes } from '../routing/mainroutes';
 import { AuthService } from '../services/auth.service';
+import { AuthgaurdService } from '../services/authgaurd.service';
 import { TokenInterceptorService } from '../services/token-interceptor.service';
 import { MasterComponent } from './master.component';
 
@@ -26,7 +27,7 @@ import { MasterComponent } from './master.component';
     RouterModule.forRoot(Mainroutes),
     HttpClientModule
   ],
-  providers: [AuthService, { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptorService, multi: true }],
+  providers: [AuthgaurdService,AuthService, { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptorService, multi: true }],
   bootstrap: [MasterComponent]
 })
 export class MasterModule { }
---
+++ Angular-Labs/sample-project/src/app/services/authgaurd.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthgaurdService implements CanActivate {
+
+  constructor(private _authService: AuthService, private _router: Router) { }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    if (this._authService.loggedIn()) {
+      return true;
+    }
+    else {
+      this._router.navigate(['login']);
+      return false;
+    }
+  }
+}
