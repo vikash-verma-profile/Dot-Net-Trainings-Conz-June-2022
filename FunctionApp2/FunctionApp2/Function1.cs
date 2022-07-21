d56fab7197eb0690427cd95968defdd6c3ed4107
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-07-21 10:04:58 +0530

    added code

---
+++ FunctionApp2/FunctionApp2/Function1.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using FunctionApp2.Models;
+using System.Linq;
+
+namespace FunctionApp2
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            SampleDbContext db = new SampleDbContext();
+            var response = db.TblSamples.ToList();
+            return new OkObjectResult(response);
+        }
+    }
+}
---
+++ FunctionApp2/FunctionApp2/Models/TblSample.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace FunctionApp2.Models
+{
+    public partial class TblSample
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
